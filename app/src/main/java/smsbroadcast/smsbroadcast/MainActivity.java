70db100de132f7894737780b8d3cf15019881367
Alan Castro <alannnc@gmail.com>
2017-10-19 18:55:35 -0500

    Added action for youtube link sms

--- app/src/main/java/smsbroadcast/smsbroadcast/MainActivity.java
+++ app/src/main/java/smsbroadcast/smsbroadcast/MainActivity.java
@@ -3,6 +3,7 @@ package smsbroadcast.smsbroadcast;
 import android.Manifest;
 import android.app.Activity;
 import android.content.ContentResolver;
+import android.content.Intent;
 import android.content.pm.PackageManager;
 import android.database.Cursor;
 import android.net.Uri;
@@ -11,6 +12,7 @@ import android.support.v4.content.ContextCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.text.Html;
+import android.util.Log;
 import android.view.View;
 import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
@@ -83,7 +85,8 @@ public class MainActivity extends Activity {
         do {
             String sender = smsInboxCursor.getString(senderIndex);
             String message = smsInboxCursor.getString(messageIndex);
-            String formattedText = String.format(getResources().getString(R.string.sms_message), sender, message);
+            //String formattedText = String.format(getResources().getString(R.string.sms_message), sender, message);
+            String formattedText = message;
             adapter.add(Html.fromHtml(formattedText).toString());
         } while (smsInboxCursor.moveToNext());
     }
@@ -97,6 +100,15 @@ public class MainActivity extends Activity {
         public void onItemClick(AdapterView<?> parent, View view, int pos, long id) {
             try {
                 Toast.makeText(getApplicationContext(), adapter.getItem(pos), Toast.LENGTH_SHORT).show();
+                Log.d("DEBUG", adapter.getItem(pos));
+                String message = adapter.getItem(pos);
+                String[] contentSms = message.split(":");
+                String typeSms = contentSms[0];
+                Uri uri = Uri.parse(contentSms[1]);
+                Log.d("DEBUG", typeSms);
+                Log.d("DEBUG", String.valueOf(uri));
+                startActivity(new Intent(Intent.ACTION_VIEW, Uri.parse("http://www.youtube.com/watch?v=cxLG2wtE7TM")));
+
             } catch (Exception e) {
                 e.printStackTrace();
             }
